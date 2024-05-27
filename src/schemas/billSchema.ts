ebdb851d56359f8edda1dfe46a8defcbe2fc02c2
KuraoHikari <dewaindra705@gmail.com>
2024-05-27 12:49:14 +0800

    Chore: add billSchema response type

--- src/schemas/billSchema.ts
+++ src/schemas/billSchema.ts
@@ -1,26 +1,55 @@
 import { z } from "zod";
+import { WarungResponseType } from "./warungSchema";
 
 const Order = z.object({
-  menuId: z.number(),
-  quantity: z.number(),
+ menuId: z.number(),
+ quantity: z.number(),
 });
 
 export const createBillSchema = z.object({
-  status: z.string(),
-  approved: z.boolean(),
-  customerName: z.string(),
+ status: z.string(),
+ approved: z.boolean(),
+ customerName: z.string(),
 
-  orders: z.array(Order),
+ orders: z.array(Order),
 });
 
 export const updateBillSchema = z.object({
-  status: z.string().optional(),
-  approved: z.boolean().optional(),
-  customerName: z.string().optional(),
+ status: z.string().optional(),
+ approved: z.boolean().optional(),
+ customerName: z.string().optional(),
 });
 
 export const createPublicBillSchema = z.object({
-  customerName: z.string(),
+ customerName: z.string(),
 
-  orders: z.array(Order),
+ orders: z.array(Order),
 });
+
+export type OrderType = {
+ id: number;
+ menuId: number;
+ billId: number;
+ quantity: number;
+ total: number;
+ createdAt: string;
+};
+
+export type BillResponseType = {
+ id: number;
+ total: number;
+ status: string;
+ approved: boolean;
+ customerName: string;
+ createdAt: string;
+ userId: number;
+ warungId: number;
+ orders: OrderType[];
+};
+
+export type getAllUserBillResponseType = BillResponseType & {
+ warung: WarungResponseType;
+};
+
+export type getBillByWarungIdResponseType = BillResponseType;
+export type updateBillResponseType = Omit<BillResponseType, "orders">;
--- src/services/billServices.ts
+++ src/services/billServices.ts
@@ -1,47 +1,57 @@
 import baseApi from "@/api/baseApi";
-import { createBillSchema, updateBillSchema } from "@/schemas/billSchema";
+import {
+ BillResponseType,
+ createBillSchema,
+ getAllUserBillResponseType,
+ getBillByWarungIdResponseType,
+ updateBillResponseType,
+ updateBillSchema,
+} from "@/schemas/billSchema";
 import { z } from "zod";
 
-export const createBill = async (data: z.infer<typeof createBillSchema>, warungId: number) => {
+export const createBill = async (
+ data: z.infer<typeof createBillSchema>,
+ warungId: number
+): Promise<BillResponseType> => {
  const response = await baseApi.post(`bill/${warungId}`, { json: data });
  if (!response.ok) {
   throw new Error("Failed to create bill");
  }
 
- const json = await response.json();
+ const json: BillResponseType = await response.json();
 
  return json;
 };
 
-export const getAllUserBill = async () => {
+export const getAllUserBill = async (): Promise<getAllUserBillResponseType> => {
  const response = await baseApi.get(`bill/all`);
  if (!response.ok) {
   throw new Error("Failed to fetch bill");
  }
 
- const json = await response.json();
+ const json: getAllUserBillResponseType = await response.json();
 
  return json;
 };
 
-export const getBillByWarungId = async (warungId: number) => {
+export const getBillByWarungId = async (warungId: number): Promise<getBillByWarungIdResponseType> => {
  const response = await baseApi.get(`bill/${warungId}`);
  if (!response.ok) {
   throw new Error("Failed to fetch bill");
  }
 
- const json = await response.json();
+ const json: getBillByWarungIdResponseType = await response.json();
 
  return json;
 };
 
-export const getBillDetailById = async (warungId: number, billId: number) => {
+export const getBillDetailById = async (warungId: number, billId: number): Promise<BillResponseType> => {
  const response = await baseApi.get(`bill/${warungId}/detail/${billId}`);
  if (!response.ok) {
   throw new Error("Failed to fetch bill");
  }
 
- const json = await response.json();
+ const json: BillResponseType = await response.json();
 
  return json;
 };
@@ -50,7 +60,7 @@ export const updateBillStatusOrApproved = async (
  data: z.infer<typeof updateBillSchema>,
  warungId: number,
  billId: number
-) => {
+): Promise<updateBillResponseType> => {
  const response = await baseApi.patch(`bill/${warungId}/edit/${billId}`, {
   json: data,
  });
@@ -58,7 +68,7 @@ export const updateBillStatusOrApproved = async (
   throw new Error("Failed to update bill");
  }
 
- const json = await response.json();
+ const json: updateBillResponseType = await response.json();
 
  return json;
 };
